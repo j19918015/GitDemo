939e539b8511fee3fd7cf43fd5b7ea35b6b6b516
Joseph <j19918015@gmail.com>
2016-08-29 17:32:58 +0800

    new version.

--- Demo/src/main/java/Test.java
+++ Demo/src/main/java/Test.java
@@ -3,13 +3,23 @@ public class Test {
 	public int plusAandB(int a , int b){
 		int c = a + b;
 		int d = a + c;
-		int e = c + d;
+		int e = c + d +5;
 		int f = e + d;
 		int g =5 +12345;
+		int x = g + f;
+		return x;
+	}
+	
+	public String plusAandB(String a,String b){
+		String c = a + b;
 		return c;
 	}
 	public void stay(){
 		int x = 15;
 		int y = x*x;
+	}
+	public static void main(String[] args){
+		Test test = new Test();
+		System.out.println(test.plusAandB(80, 7));
 	}
 }
